f4c593330028324e4488382c917656a0680d0eb1
AlbertoForesti <alberto.foresti.01@icloud.com>
2024-01-02 11:40:57 +0100

    Causal augmentation semi ok, codice scritto da cani

--- kernel.py
+++ kernel.py
@@ -1,32 +1,28 @@
-
-from typing import Union, Callable, Iterable, Optional, Any, Tuple, List
+from typing import Union, Callable, Iterable, Optional, Any, Tuple, List, Hashable
+from tqdm import tqdm
 import numpy as np
 import pandas as pd
 import xgboost as xgb
+import networkx as nx
+import seaborn as sns
+from abc import ABC, abstractmethod
+from copy import copy
+from scipy.stats import gaussian_kde
 
-class ProductKernel:
+class BaseKernel(ABC):
 
-    """
-    Implements a product kernel given a list of kernel functions
-    """
-    
     def __init__(self) -> None:
         pass
-
-    def fit(self, X: np.ndarray, kernel_functions: Optional[Iterable[Callable]], kernel_coefficients: Optional[Union[Iterable[float], str]] = None):
-        self.X = X
-        if kernel_functions is None:
-            kernel_functions = self.infer_kernel_from_data(X)
-
-        if isinstance(kernel_coefficients, str):
-            if kernel_coefficients == 'silverman':
-                kernel_coefficients = self.infer_kernel_coefficients_from_data(X)
-            else:
-                kernel_coefficients = None
-        kernel = lambda x: np.average(np.exp(sum([np.log((kf((x-X[:,i])/kernel_coefficients[i]))/kernel_coefficients[i]) for i, kf in enumerate(kernel_functions)])))
-        self._kernel = kernel
     
-    def infer_kernel_coefficients_from_data(X: Union['pd.DataFrame',np.ndarray]) -> List[Callable]:
+    @abstractmethod
+    def fit(self, X: np.ndarray, kernel_functions: Optional[Iterable[Callable]], kernel_coefficients: Optional[Union[Iterable[float], str]] = None):
+        raise NotImplementedError(f'Fit must be implemented')
+    
+    @abstractmethod
+    def predict(self, predt: np.ndarray):
+        raise NotImplementedError(f'Predict must be implemented')
+
+    def infer_kernel_coefficients_from_data(self, X: Union['pd.DataFrame',np.ndarray]) -> List[Callable]:
         types = X.dtypes
         X = np.array(X)
         coeffs = []
@@ -36,32 +32,292 @@ class ProductKernel:
             elif pd.api.types.is_float_dtype(t):
                 coeffs.append(1.06*np.var(X[:,i])*(len(X))**(-0.2)) # Silverman rule of thumb
             else:
-                raise TypeError(f'{t} not supported')
+                raise TypeError(f'{t} not supported {X[:,i]}')
         return coeffs
 
-    def infer_kernel_from_data(X: Union['pd.DataFrame',np.ndarray]) -> List[Callable]:
+    def infer_kernel_from_data(self, X: Union['pd.DataFrame',np.ndarray]) -> List[Callable]:
         types = X.dtypes
         kfs = []
-        for t in types:
+        for i, t in enumerate(types):
             if pd.api.types.is_integer_dtype(t):
-                kfs.append(lambda x: 1 if x==0 else 0)
+                kfs.append(lambda x: np.array([1 if x_i==0 else 0 for x_i in x]))
             elif pd.api.types.is_float_dtype(t):
                 kfs.append(lambda x: np.power(2*np.pi, -0.5)*np.exp(-np.power(x, 2)/2))
             else:
-                raise TypeError(f'{t} not supported')
+                raise TypeError(f'{t} not supported {X[:,i]}')
         return kfs
 
+class ProductKernel(BaseKernel):
+
+    """
+    Implements a product kernel given a list of kernel functions
+    """
+    
+    def __init__(self) -> None:
+        pass
+
+    def base_kf(self, x) -> Any:
+        prod = 1
+        x = np.array(x).reshape((x.shape[0], -1))
+        for feat in range(x.shape[1]):
+            coeff = self.kernel_coefficients[feat]
+            try:
+                partial_res = self.kernel_functions[feat](x[:,feat]/coeff)/coeff
+            except:
+                raise IndexError(f'Out of bound: sample={str(x)}')
+            prod*=partial_res
+        return prod
+
+    def fit(self, X: np.ndarray, kernel_functions: Optional[Iterable[Callable]] = None, kernel_coefficients: Optional[Union[Iterable[float], str]] = None):
+        self.X = np.array(X)
+        if kernel_functions is None:
+            self.kernel_functions = self.infer_kernel_from_data(X)
+
+        if isinstance(kernel_coefficients, str):
+            if kernel_coefficients == 'silverman':
+                self.kernel_coefficients = self.infer_kernel_coefficients_from_data(X)
+            else:
+                self.kernel_coefficients = None
+        else:
+            self.kernel_coefficients = self.infer_kernel_coefficients_from_data(X)
+        kernel = lambda x: np.average(self.base_kf(x-self.X))
+        kernel = np.vectorize(kernel)
+        self._kernel = kernel
+        self._kernel = gaussian_kde(self.X.reshape((-1,)), bw_method='silverman')
+
     def predict(self, predt: np.ndarray):
+        print('predt is ', predt)
+        print('ret is ', self._kernel(predt))
         return self._kernel(predt)
     
     def gradient(self, predt: np.ndarray, dtrain: xgb.DMatrix) -> np.ndarray:
         '''Compute the gradient squared error for estimated probabilities.'''
         y = dtrain.get_label()
-        return self.predict(predt)-y
+        ret = self.predict(predt).reshape((-1,))-y
+        print('y is ', y)
+        return ret
     
     def hessian(self, predt: np.ndarray, dtrain: xgb.DMatrix) -> np.ndarray:
         '''Compute the gradient squared error for estimated probabilities.'''
-        return np.ones_like(predt)
+        return np.ones_like(dtrain.get_label())
 
-    def __call__(self, x) -> Any:
-        return self._kernel(x)
+    def __call__(self, y_pred, y_true) -> Any:
+        return self.gradient(y_pred, y_true), self.hessian(y_pred, y_true)
+
+
+class GraphKernel(BaseKernel):
+
+    def __init__(self) -> None:
+        pass
+
+    def fit(self, X: np.ndarray, causal_graph: 'nx.Digraph', kernel_functions: Optional[Iterable[Callable]] = None, kernel_coefficients: Optional[Union[Iterable[float], str]] = None):
+        self.X = X
+
+        if kernel_functions is None:
+            self.kernel_functions = self.infer_kernel_from_data(X)
+
+        if isinstance(kernel_coefficients, str):
+            if kernel_coefficients == 'silverman':
+                self.kernel_coefficients = self.infer_kernel_coefficients_from_data(X)
+            else:
+                self.kernel_coefficients = None
+        
+        feature_list = list(nx.topological_sort(causal_graph))
+
+        nx.set_node_attributes(causal_graph, {feat: lambda x: self.base_kf(x, feat, causal_graph) for feat in feature_list}, name='kernel')
+        
+        self._causal_graph = causal_graph
+    
+    def base_kf(self, x, feature: int, causal_graph: 'nx.Digraph') -> Any:
+        preceding_features = causal_graph.predecessors(feature)
+        prod = 1
+        x = np.array(x)
+        for feat in preceding_features:
+            coeff = self.kernel_coefficients[feat]
+            try:
+                partial_res = self.kernel_functions[feat](x[feat]/coeff)/coeff
+            except:
+                raise IndexError(f'Out of bound: current feature={str(feat)}, sample={str(x)}, preceding features={str(list(preceding_features))}, origin feature={feature}, feature topo order = {list(nx.topological_sort(causal_graph))}, feature preceding 2 = {list(causal_graph.predecessors(2))}')
+            prod*=partial_res
+        return prod
+
+    def predict(self, predt: np.ndarray, feat: Hashable):
+        return self._causal_graph.nodes[feat]['kernel'](predt)
+    
+    def __call__(self, predt: np.ndarray, feat: Hashable):
+        return self._causal_graph.nodes[feat]['kernel'](predt)
+
+def map_indeces_to_sample(indeces: Iterable, feature_list: Iterable, data: Iterable) -> 'np.Array':
+    try:
+        data = np.array(data)
+    except:
+        TypeError(f'Could not cast data of type {type(data)} to numpy array')
+    try:
+        feature_list = np.array(feature_list)
+    except:
+        TypeError(f'Could not cast feature_list of type {type(feature_list)} to numpy array')
+    try:
+        indeces = np.array(list(indeces))
+    except:
+        TypeError(f'Could not cast indeces of type {type(indeces)} to numpy array')
+    if len(indeces.shape)==1:
+        indeces = np.reshape(indeces, (1,)+indeces.shape)
+    return np.array([[data[sample_index, feature_list[i]] for i, sample_index in enumerate(indeces_of_one_sample)]for indeces_of_one_sample in indeces]) 
+
+def train_domain_shifter(d1: 'pd.DataFrame', d2: 'pd.DataFrame'):
+    regressors = []
+    types = np.array(d2.dtypes)
+    d1 = copy(d1)
+    for i, column in enumerate(d2.columns):
+        if not pd.api.types.is_integer_dtype(types[i]) and not pd.api.types.is_float_dtype(types[i]):
+            d1 = d1.drop(column, axis=1)
+            continue
+        pk = ProductKernel()
+        print(f'Fitting {column}')
+        pk.fit(pd.DataFrame(d2[column]))
+        pk_d1 = ProductKernel()
+        pk_d1.fit(pd.DataFrame(d1[column]))
+        y_train = np.array(d2[column].sample(len(d1), replace=True))
+        # y_train = pk.predict(y_train)
+        dtrain = xgb.DMatrix(d1, y_train)
+        reg = xgb.train({'tree_method': 'hist', 'seed': 1994, 'eta': 1e-1},  # any other tree method is fine.
+           dtrain=dtrain,
+           num_boost_round=10,
+           obj=pk)
+        regressors.append(reg)
+    return regressors
+
+def shift_domain(df: 'pd.DataFrame', regressors: Iterable):
+    df_shifted = copy(df)
+    for i, reg in enumerate(regressors):
+        df_shifted.iloc[:,i] = reg.predict(xgb.DMatrix(df))
+    return df_shifted
+
+def check_sample_is_original(indeces: Iterable):
+    return len(np.unique(indeces)) == 1
+
+def causal_augmentation(data: 'pd.DataFrame', causal_graph: nx.Graph, kernel: GraphKernel, threshold: float, aug_coeff: float, sample_size: int = 10) -> Tuple['np.array', 'np.array']:
+
+    weight_dict = dict()
+    weight_dict[tuple()] = 1
+    feature_list = list(nx.topological_sort(causal_graph))
+    data = np.array(data.sample(sample_size))
+    i = -1
+
+    for feature in tqdm(feature_list):
+        i+=1
+        new_dict = dict()
+        for leaf in tqdm(weight_dict.keys()):
+            kernel_results = []
+            augmented_sample = np.zeros_like(feature_list)
+            for i in range(len(leaf)):
+                augmented_sample[feature_list[i]] = data[leaf[i], feature_list[i]]
+            for k in range(len(data)):
+                truncated_data = np.zeros_like(feature_list)
+                for i in range(len(leaf)):
+                    truncated_data[feature_list[i]] = data[k, feature_list[i]]
+                try:
+                    # kernel_results.append(kernel(augmented_sample-truncated_data, feature))
+                    kernel_results.append(kernel(augmented_sample-truncated_data, feature))
+                except:
+                    raise ValueError(f'Shapes {augmented_sample.shape}, {truncated_data.shape} not compatible\
+                                        \nSamples are {augmented_sample}, {truncated_data}')
+            den = sum(kernel_results)
+            for current_datum_index in range(len(data)):
+                new_leaf = leaf + (current_datum_index,)
+                num = kernel_results[current_datum_index]
+                if den>0:
+                    w = num/den
+                else:
+                    w = 0
+                w = weight_dict[leaf]*w
+                if w>threshold/len(data):
+                    new_dict[new_leaf] = w
+                
+        weight_dict = new_dict
+    
+    augmented_data_set = map_indeces_to_sample(weight_dict.keys(), feature_list, data)
+    augmented_data_set = np.vstack((data, augmented_data_set))
+
+    weights_aug = aug_coeff*len(data)*np.array(list(weight_dict.values()))
+    weights_orig = (1-aug_coeff)*np.ones((len(data),))
+    weights = np.hstack((weights_orig, weights_aug))
+
+    return augmented_data_set, weights
+
+def fast_causal_augmentation(data: 'pd.DataFrame', causal_graph: nx.Graph, kernel: GraphKernel, threshold: float, aug_coeff: float, sample_size: int = 10, topk: Optional[int] = None) -> Tuple['np.array', 'np.array']:
+
+    weight_dict = dict()
+    weight_dict[tuple()] = 1
+    feature_list = list(nx.topological_sort(causal_graph))
+    columns = None
+    if isinstance(data, pd.DataFrame):
+        columns = data.columns
+    data = np.array(data.sample(sample_size))
+    i = -1
+
+    for feature in tqdm(feature_list):
+        i+=1
+        new_dict = dict()
+        for leaf in weight_dict.keys():
+            feature_values = list()
+            kernel_results = []
+            augmented_sample = np.zeros_like(feature_list)
+            for i in range(len(leaf)):
+                augmented_sample[feature_list[i]] = data[leaf[i], feature_list[i]]
+            for k in range(len(data)):
+                truncated_data = np.zeros_like(feature_list)
+                for i in range(len(leaf)):
+                    truncated_data[feature_list[i]] = data[k, feature_list[i]]
+                try:
+                    # kernel_results.append(kernel(augmented_sample-truncated_data, feature))
+                    kernel_results.append(kernel(augmented_sample-truncated_data, feature))
+                except:
+                    raise ValueError(f'Shapes {augmented_sample.shape}, {truncated_data.shape} not compatible\
+                                        \nSamples are {augmented_sample}, {truncated_data}')
+            den = sum(kernel_results)
+            for current_datum_index in range(len(data)):
+                if data[current_datum_index, feature] in feature_values:
+                    continue
+                else:
+                    feature_values.append(data[current_datum_index, feature])
+                new_leaf = leaf + (current_datum_index,)
+                num = kernel_results[current_datum_index]
+                if den>0:
+                    w = num/den
+                else:
+                    w = 0
+                w = weight_dict[leaf]*w
+                if w>threshold/len(data):
+                    new_dict[new_leaf] = w
+            if topk is not None:
+                new_dict = dict(sorted(new_dict.items(), key=lambda x: x[1], reverse=True)[:topk])
+                
+        weight_dict = new_dict
+    
+    if len(weight_dict.keys())==0:
+        augmented_data_set = np.empty_like(data)
+    augmented_data_set = map_indeces_to_sample(weight_dict.keys(), feature_list, data)
+    augmented_data_set = np.vstack((data, augmented_data_set))
+
+    weights_aug = aug_coeff*len(data)*np.array(list(weight_dict.values()))
+    weights_orig = (1-aug_coeff)*np.ones((len(data),))
+    weights = np.hstack((weights_orig, weights_aug))
+
+    if columns is not None:
+        return pd.DataFrame(augmented_data_set, columns=columns), weights
+
+    return augmented_data_set, weights
+
+
+def get_default_kernel_functions_from_df(df: 'pd.DataFrame') -> List[Callable]:
+    types = df.dtypes
+    kfs = []
+    for t in types:
+        if pd.api.types.is_integer_dtype(t):
+            kfs.append(lambda x: 1 if x==0 else 0)
+        elif pd.api.types.is_float_dtype(t):
+            kfs.append(lambda x: np.power(2*np.pi, -0.5)*np.exp(-np.power(x, 2)/2))
+        else:
+            raise TypeError(f'{t} not supported')
+    return kfs
